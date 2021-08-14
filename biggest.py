b721ec9314d116a24aeed45853318f4d2437aef7
Meral <hi.meral@gmail.com>
2021-08-14 21:02:20 +0530

    14 aug

--- biggest.py
+++ biggest.py
@@ -4,9 +4,11 @@ while True:
     x = (input("Insert : "))
 
     if (x.isdigit()):
+
         x = int(x)
         if(x > max):
             max = x
     else:
+
         print(f"Biggest number is {max}")
         break
