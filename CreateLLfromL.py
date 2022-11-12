9409e23dbf5123c6175c684f0ac186c7259e5750
Meral <miral.maradia@ccmllc.com>
2022-11-12 12:06:19 +0530

    Linked List

--- CreateLLfromL.py
+++ CreateLLfromL.py
@@ -3,7 +3,16 @@ class ListNode:
 
         self.val = A.pop(0)
         self.next = None
+
         if len(A) == 0:
             return
 
         self.next = ListNode(A)
+
+
+class showLL:
+    def __init__(self, LL):
+
+        while LL is not None:
+            print(LL.val)
+            LL = LL.next
